2c003059b3b73dc5320c91c72ca87b4c713f498f
Prudent NGAIPERA-DONG <111011332+Prudent777@users.noreply.github.com>
2023-01-27 12:16:27 +0100

    Add files via upload

---
+++ 5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  * takes no args
+  *
+  * Description: used printf(); function to print to stdout
+  * Return: Program always return 0(success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
